3516deb435697d7fa0ae06a1198e9aae98235ea4
Lidia Alonso <l.alonsodiaz@gmail.com>
2021-01-13 15:09:58 +0100

    added sound

--- script.js
+++ script.js
@@ -6,5 +6,27 @@ sounds.forEach((sound) => {
 	//loop over each sound array
 	btn.innerText = sound;
 	//  <div id="buttons"></div>;
+
+	btn.addEventListener('click', () => {
+		//id is same as sound
+		//method play
+
+		//function call stopSongs
+		stopSongs();
+
+		document.getElementById(sound).play();
+	});
+
 	document.getElementById('buttons').appendChild(btn);
 });
+
+// function stopSongs
+function stopSongs() {
+	sounds.forEach((sound) => {
+		const song = document.getElementById(sound);
+
+		//method pause
+		song.pause();
+		song.currentTime = 0;
+	});
+}
